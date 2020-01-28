106b024742d2c84bceeeac517e8c16f2423b7a1f
Jeremy Morales <jeremy.morales@gmail.com>
2020-01-28 12:45:54 -0800

    assets added to folders

---
+++ nail-lift-fe/src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './images/NailLift_LogoForWeb.png';
+
+function Header() {
+    return (
+        <div>
+            <img src={logo} alt="nail-logo" />
+        </div>
+    );
+}
+
+export default Header;
